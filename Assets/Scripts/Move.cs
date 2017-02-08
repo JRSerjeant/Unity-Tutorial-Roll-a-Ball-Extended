503d36c5f3332381310aa2deb219206f208d65e9
Jack Serjeant <Jack.Serjeant@gmail.com>
2017-02-08 22:08:46 +0000

    Move Pick Ups

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject ground;
+    private Renderer groundRenderer;
+    private float groundBoundsMinX;
+    private float groundBoundsMaxX;
+    private float groundBoundsMinZ;
+    private float groundBoundsMaxZ;
+    public Vector3 targetPostion;
+    public float Speed;
+
+
+    private void Start()
+    {
+        groundRenderer = ground.GetComponent<Renderer>();
+
+        groundBoundsMinX = ground.transform.position.x - groundRenderer.bounds.extents.x;
+        groundBoundsMaxX = ground.transform.position.x + groundRenderer.bounds.extents.x;
+
+        groundBoundsMinZ = ground.transform.position.z - groundRenderer.bounds.extents.z;
+        groundBoundsMaxZ = ground.transform.position.z + groundRenderer.bounds.extents.z;
+
+        targetPostion = NewTargetPosition();
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPostion, Speed * Time.deltaTime);
+
+        if(transform.position == targetPostion)
+        {
+            targetPostion = NewTargetPosition();
+        }
+    }
+
+    Vector3 NewTargetPosition()
+    {
+        float randomX = Random.Range(groundBoundsMinX, groundBoundsMaxX);
+        float randomZ = Random.Range(groundBoundsMinZ, groundBoundsMaxZ);
+        return new Vector3(randomX, 0.5f, randomZ);
+    }
+
+}
