d6cca109b21c5a6fbffea884b77ff45a95f60bc0
Jack Serjeant <Jack.Serjeant@gmail.com>
2017-02-12 17:59:48 +0000

    Bullet Collision

---
+++ Assets/OnCollisionDestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollisionDestroy : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "Player":
+                Destroy(gameObject);
+                break;
+            case "Wall":
+                Destroy(gameObject);
+                break;
+            default:
+                break;
+        }
+    }
+}
