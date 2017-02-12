77d0affdcbdc54bb86b43772e2fe3c6697efb554
Jack Serjeant <Jack.Serjeant@gmail.com>
2017-02-12 12:14:51 +0000

    Movement of bullets

---
+++ Assets/Scripts/FireBullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBullet : MonoBehaviour {
+
+    public GameObject Bullet;
+    public int Quantity;
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float Chance = Random.Range(0.0f, 10.0f);
+        if (Chance < 0.025f)
+        {
+            Instantiate(Bullet, gameObject.transform.position, Quaternion.identity);
+        }
+	}
+}
---
+++ Assets/Scripts/PointAtTarget.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAtTarget : MonoBehaviour {
+
+    GameObject Target;
+
+    private void Start()
+    {
+        Target = GameObject.FindGameObjectWithTag("Player");
+        transform.LookAt(Target.transform);
+    }
+}
