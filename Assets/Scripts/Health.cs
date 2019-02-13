572ca45124ac532613109947e3dcd27cccc12127
Jack Serjeant <Jack.Serjeant@gmail.com>
2019-02-13 12:20:30 +0000

    Adding Health progress.

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Enemy")
+        {
+            health = health - other.gameObject.GetComponent<Attack>().damage;
+        }
+    }
+
+}
